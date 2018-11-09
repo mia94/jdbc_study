d8686c4537c2211c0bcaff32f3d2e5719ade14d7
mia94 <mia_3@hanmail.net>
2018-11-09 16:26:08 +0900

    DepartmentDaoImpl 코드 간소화

--- src/jdbc_study/dao/DepartmentDaoImpl.java
+++ src/jdbc_study/dao/DepartmentDaoImpl.java
@@ -20,37 +20,24 @@ public class DepartmentDaoImpl implements DepartmentDao {
 	public List<Department> selectDepartmentByAll(){//exception빨간줄 뜬거 dao로 넘기지X
 		List<Department> list = new ArrayList<>();
 		String sql = "select deptno, deptname, floor from department";//select했을때의 컬럼부른 순서가 중요함!!
-		Connection conn = null;//java.sql connection으로 import
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;//ResultSet import 시키기 (한개뿐)
 		
-		try {//예외처리 여기서 하기
-			conn = MySQLJdbcUtil.getConnection();
-			pstmt = conn.prepareStatement(sql);//conn.해서 나오는 첫번째꺼
-			LOG.debug(pstmt);//debug매개변수 한개있는걸로 선택, 반드시 여기서 pstmt 확인해봐야 함
-			rs = pstmt.executeQuery();//rs가 커서와 같아서 컬럼명을 가르킴, 쿼리는 insert에 이용됨
-
+		try(Connection conn = MySQLJdbcUtil.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()){
 			while(rs.next()) {//다음행이 있으면 true
-				int deptNo = rs.getInt("deptno");//컬럼이름을 적어주는게 편함, select한 순서대로 적어야함
-				String deptName = rs.getString("deptname");
-				int floor = rs.getInt("floor");
-				Department dept = new Department(deptNo, deptName, floor);
-				list.add(dept);
+				list.add(getDepartment(rs));
 			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {//거꾸로 종료시키기
-			try {//예외처리해주기
-				rs.close();
-				pstmt.close();
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
+		} catch (SQLException e1) {
+			e1.printStackTrace();
 		}
 		return list;//리스트를 리턴
+	}
+
+	private Department getDepartment(ResultSet rs) throws SQLException {
+		int deptNo = rs.getInt("deptno");
+		String deptName = rs.getString("deptName");
+		int floor = rs.getInt("floor");
+		return new Department(deptNo, deptName, floor);
 	}
 
 }
